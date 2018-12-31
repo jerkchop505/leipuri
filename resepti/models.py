ab00428200ff4108925e88268efd2e3c488a177c
jerkchop505 <jerkchop505@outlook.com>
2018-12-31 11:04:07 -0800

    Further migrations, narrowing down on a basic schema to create a recipe

--- resepti/models.py
+++ resepti/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=150)
-    category = models.CharField(max_length=150)
-    servings = models.IntegerField()
-    ingredients = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
-    steps = models.ForeignKey('Step', on_delete=models.CASCADE)
-
-
 class Ingredient(models.Model):
-    name = models.CharField(max_length=150)
-    itype = models.CharField(max_length=150)
+    name = models.CharField(max_length=255)
+    itype = models.CharField(max_length=255)
+    # 1 weight unit is 1 milligram
+    weight = models.IntegerField(null=True)
+    # 1 volume unit is 1 microliter
+    volume = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Step(models.Model):
     number = models.IntegerField()
     text = models.CharField(max_length=255)
+    recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    servings = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
