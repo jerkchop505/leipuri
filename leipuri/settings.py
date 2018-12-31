b590f22e00a8bd73a0395357244efe881b25f9f7
jerkchop505 <jerkchop505@outlook.com>
2018-12-30 20:46:48 -0800

    db setup and migrated

--- leipuri/settings.py
+++ leipuri/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'leipuri.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leipuri',
+        'USER': 'monty',
+        'PASSWORD': 'some_pass',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- resepti/models.py
+++ resepti/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    servings = models.IntegerField()
+    ingredients = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    steps = models.ForeignKey('Step', on_delete=models.CASCADE)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=150)
+    itype = models.CharField(max_length=150)
+
+
+class Step(models.Model):
+    number = models.IntegerField()
+    text = models.CharField(max_length=255)
