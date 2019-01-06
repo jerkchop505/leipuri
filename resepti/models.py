80276b1f7e1e692c635363d986dc2eac26aeefad
jerkchop505 <jerkchop505@outlook.com>
2019-01-05 18:39:35 -0800

    updated models to be more better

--- resepti/models.py
+++ resepti/models.py
@@ -4,18 +4,6 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     itype = models.CharField(max_length=255)
-    # 1 weight unit is 1 gram
-    weight = models.DecimalField(
-        decimal_places=2,
-        max_digits=7,
-        null=True
-    )
-    quantity = models.DecimalField(
-        decimal_places=2,
-        max_digits=7,
-        null=True
-    )
-    recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -28,6 +16,29 @@ class Step(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class IngredAmount(models.Model):
+    # 1 weight unit is 1 gram
+    weight = models.DecimalField(
+        decimal_places=2,
+        max_digits=7,
+        null=True
+    )
+    quantity = models.DecimalField(
+        decimal_places=2,
+        max_digits=7,
+        null=True
+    )
+    recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
+    ing = models.ForeignKey('Ingredient', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        if (self.ing.weight is None):
+            ingred = str(self.ing.quantity) + " "
+        else:
+            ingred = str(self.ing.weight) + "g "
+        return ingred + self.ing.name
 
 
 class Recipe(models.Model):
